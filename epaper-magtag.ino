ecf4a9c27c43f4172835e8ba98c5428e1ea1246b
Alex Lelievre <alex@folabs.com>
2021-09-13 12:53:32 -0700

    Added when the last snack or food was consumed.

--- epaper-magtag.ino
+++ epaper-magtag.ino
@@ -53,8 +53,8 @@
 #define BATTERY_VID_THRESHOLD 50   // percent
 #define LOW_BATTERY_THRESHOLD 10   // percent
 
-#define PIXEL_COUNT  4     // Number of NeoPixels
-#define PIXEL_BRIGHTNESS 32
+#define PIXEL_COUNT       4     // Number of NeoPixels
+#define PIXEL_BRIGHTNESS 16
 
 #define BUTTON_A GPIO_NUM_15
 #define BUTTON_B GPIO_NUM_14
@@ -70,6 +70,30 @@
 #define kFoodIconHeight 74
 
 
+struct Button 
+{
+    const uint8_t PIN;
+    bool          pressed;
+};
+
+
+typedef struct
+{
+  uint8_t hour; 
+  uint8_t min;
+  uint8_t sec;
+  uint8_t day;
+} timeRecord;
+
+enum button_state 
+{ 
+  kFoodSnackBit     = 1 << 0, 
+  kFoodBreakfastBit = 1 << 1, 
+  kFoodLunchBit     = 1 << 2, 
+  kFoodDinnerBit    = 1 << 3 
+};
+
+
 enum alignmentType {LEFT, RIGHT, CENTER};
 
 static const char*    ssid     = STASSID;
@@ -83,26 +107,18 @@ static int         daylightOffset_sec = 3600;   // In the US/UK DST is +1hr or 3
 static long s_sleep_duration_mins = 60;  // Sleep time in minutes (1 hour so that we can reset the states promptly at midnight)
 static int  s_wifi_signal = 0;
 
-static String  s_date_str;
-static int     s_last_update_Hour = 0, s_last_update_Min = 0, s_last_update_Sec = 0, s_last_update_Day = 0;
-static long    StartTime = 0;
+static String     s_date_str;
+static timeRecord s_last_update    = {0};
+static timeRecord s_last_snack     = {0};
+static timeRecord s_last_breakfast = {0};
+static timeRecord s_last_lunch     = {0};
+static timeRecord s_last_dinner    = {0};
 
-static int     tempOffsetX = 85;
-static int     kTimeoutThresholdSecs = 8; // seconds
+static int kTimeoutThresholdSecs   = 8; // seconds
 
-enum button_state 
-{ 
-  kFoodSnackBit     = 1 << 0, 
-  kFoodBreakfastBit = 1 << 1, 
-  kFoodLunchBit     = 1 << 2, 
-  kFoodDinnerBit    = 1 << 3 
-};
 RTC_DATA_ATTR uint8_t rtc_button_state = 0;
+RTC_DATA_ATTR uint8_t rtc_snack_count  = 0;
 
-struct Button {
-    const uint8_t PIN;
-    bool          pressed;
-};
 
 RTC_DATA_ATTR Button button1 = { BUTTON_A, false };
 RTC_DATA_ATTR Button button2 = { BUTTON_B, false };
@@ -114,7 +130,11 @@ static const char* kFoodBreakfast = "Breakfast";
 static const char* kFoodLunch     = "Lunch";
 static const char* kFoodDinner    = "Dinner";
 
+static bool s_idle = false;
+
+#ifdef SLEEP_ENABLED
 RTC_DATA_ATTR bool rtc_booted = false;
+#endif
 
 // static classes
 Adafruit_NeoPixel strip(PIXEL_COUNT, NEOPIX_PIN, NEO_GRB + NEO_KHZ800);
@@ -138,6 +158,26 @@ void drawString(int x, int y, String text, alignmentType alignment)
   if (alignment == CENTER) x = x - w / 2;
   u8g2Fonts.setCursor(x, y + h);
   u8g2Fonts.print(text);
+}
+
+
+void drawTime( int x, int y, timeRecord* tm )  
+{
+  int16_t  x1, y1; //the bounds of x,y and w and h of the variable 'text' in pixels.
+  uint16_t w, h;
+
+  char update_time[30];
+  struct tm timeInfo = {0};
+  timeInfo.tm_hour = tm->hour;
+  timeInfo.tm_min  = tm->min;
+  timeInfo.tm_sec  = tm->sec;
+  timeInfo.tm_mday = tm->day;
+  strftime( update_time, sizeof( update_time ), "%H:%M", &timeInfo );
+  
+  display.setTextWrap( false );
+  display.getTextBounds( update_time, x, y, &x1, &y1, &w, &h );
+  u8g2Fonts.setCursor( x, y + h );
+  u8g2Fonts.print( update_time );
 }
 
 
@@ -174,16 +214,16 @@ boolean UpdateLocalTime()
   }
 
   // check to see if we moved onto a new day...
-  if( s_last_update_Day && (s_last_update_Day != timeinfo.tm_mday) )
+  if( s_last_update.day && (s_last_update.day != timeinfo.tm_mday) )
   {
     // reset all the icons...
     rtc_button_state = 0;
   }
   
-  s_last_update_Hour = timeinfo.tm_hour;
-  s_last_update_Min  = timeinfo.tm_min;
-  s_last_update_Sec  = timeinfo.tm_sec;
-  s_last_update_Day  = timeinfo.tm_mday;
+  s_last_update.hour = timeinfo.tm_hour;
+  s_last_update.min  = timeinfo.tm_min;
+  s_last_update.sec  = timeinfo.tm_sec;
+  s_last_update.day  = timeinfo.tm_mday;
   Serial.println( &timeinfo, "%a %b %d %Y   %H:%M:%S" );      // Displays: Saturday, June 24 2017 14:05:49
   
   // see http://www.cplusplus.com/reference/ctime/strftime/
@@ -203,14 +243,14 @@ bool CheckForTimeout()
     return false;
   }
 
-  // do a simple compare
-  if( s_last_update_Hour != timeinfo.tm_hour )
+  // do a simple compare -- change this to simply convert everything to seconds and compare those... !!@
+  if( s_last_update.hour != timeinfo.tm_hour )
     return true;
 
-   if( (s_last_update_Hour == timeinfo.tm_hour) && (timeinfo.tm_min - s_last_update_Min > 1) )
+   if( (s_last_update.hour == timeinfo.tm_hour) && (timeinfo.tm_min - s_last_update.min > 1) )
     return true;
 
-   if( (s_last_update_Min == timeinfo.tm_min) && (timeinfo.tm_sec - s_last_update_Sec > kTimeoutThresholdSecs) )
+   if( (s_last_update.min == timeinfo.tm_min) && (timeinfo.tm_sec - s_last_update.sec > kTimeoutThresholdSecs) )
     return true;
 
   return false;
@@ -231,6 +271,9 @@ void Blink(byte PIN, int DELAY_MS, byte loops)
 
 void GoIdle()
 {
+  if( s_idle )
+    return;
+    
   // turn off the led if on
   digitalWrite( LED_BUILTIN, HIGH );
 
@@ -238,6 +281,7 @@ void GoIdle()
   strip.show();
 
   Serial.println( "Going idle..." );
+  s_idle = true;
 }
 
 
@@ -261,36 +305,40 @@ void BeginSleep()
 }
 
 
-
-
-
-void ARDUINO_ISR_ATTR isr(void* arg) {
+void ARDUINO_ISR_ATTR isr( void* arg ) 
+{
     Button* s = static_cast<Button*>(arg);
     s->pressed = true;
 }
 
 
-void SetNeoPixelsColor(uint32_t color) 
+void SetNeoPixelsColor( uint32_t color ) 
 {
-  for(int i=0; i<strip.numPixels(); i++) {  // For each pixel in strip...
+  for( int i=0; i < strip.numPixels(); i++ ) 
     strip.setPixelColor(i, color);          //  Set pixel's color (in RAM)
-  }
+  
   strip.show();
 }
+
 
 // Theater-marquee-style chasing lights. Pass in a color (32-bit value,
 // a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
 // between frames.
-void theaterChase(uint32_t color, int wait) {
-  for(int a=0; a<10; a++) {  // Repeat 10 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+void theaterChase( uint32_t color, int wait ) 
+{
+  for( int a = 0; a < 10; a++ ) 
+  {  
+    // Repeat 10 times...
+    for( int b = 0; b < 3; b++ ) 
+    { 
+      //  'b' counts from 0 to 2...
       strip.clear();         //   Set all pixels in RAM to 0 (off)
       // 'c' counts up from 'b' to end of strip in steps of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-      }
+      for( int c = b; c < strip.numPixels(); c += 3 ) 
+        strip.setPixelColor( c, color ); // Set pixel 'c' to value 'color'
+
       strip.show(); // Update strip with new contents
-      delay(wait);  // Pause for a moment
+      delay( wait );  // Pause for a moment
     }
   }
 }
@@ -316,7 +364,6 @@ bool GetIconState( uint8_t foodBit )
 
 void setup(void)
 {
-  StartTime = millis();
   Serial.begin( 115200 );
 //  while (!Serial);   // time to get serial running
 
@@ -383,31 +430,126 @@ void loop()
   {
 #ifndef SLEEP_ENABLED
     SetNeoPixelsColor( strip.Color(127, 127, 127) );
+    s_idle = false;
 #endif    
     UpdateLocalTime();
 
     // toggle the states
     if( button1.pressed )
-      UpdateIconState( kFoodSnackBit, !GetIconState( kFoodSnackBit ) );
+    {
+      s_last_snack = s_last_update;
+      ++rtc_snack_count;
+      UpdateIconState( kFoodSnackBit, rtc_snack_count );
+       button1.pressed = false;
+    }      
     else if( button2.pressed )
+    {
+      s_last_breakfast = s_last_update;
       UpdateIconState( kFoodBreakfastBit, !GetIconState( kFoodBreakfastBit ) );
+      button2.pressed = false;
+    }
     else if( button3.pressed )
+    {
+      s_last_lunch = s_last_update;
       UpdateIconState( kFoodLunchBit, !GetIconState( kFoodLunchBit ) );
+      button3.pressed = false;
+    }
     else if( button4.pressed )
+    {
+      s_last_dinner = s_last_update;
       UpdateIconState( kFoodDinnerBit, !GetIconState( kFoodDinnerBit ) );
+      button4.pressed = false;
+    }
   }
-
-  // reset the buttons...
-  button1.pressed = false;
-  button2.pressed = false;
-  button3.pressed = false;
-  button4.pressed = false;
 
   // after no activity, go idle-- !!@ in the future if we can detect we are on a battery, we would sleep here...
   if( CheckForTimeout() )
     GoIdle();
 }
 
+
+void draw_epd( bool draw_date )
+{
+  Serial.println("EPD begun");
+
+  display.begin();
+  display.clearBuffer();
+  display.fillScreen( EPD_WHITE );
+
+  display.drawBitmap( 0, -1, s_folabs_logo, 128, 32, EPD_BLACK );
+
+  u8g2Fonts.setFont( u8g2_font_helvB10_tf );
+  drawString( 40, 12, "Far Out Labs", LEFT );
+
+//  u8g2Fonts.setForegroundColor( EPD_RED );
+//  u8g2Fonts.setFont( u8g2_font_helvR08_tf );
+//  drawString( 40 + 120, 10, "K6LOT-13", LEFT );
+
+  u8g2Fonts.setForegroundColor( EPD_BLACK );
+  u8g2Fonts.setFont( u8g2_font_helvR08_tf );
+
+  uint16_t w = getTextWidth( "Snack" );
+  uint16_t centered = (kFoodIconWidth - w) / 2;
+  const uint8_t* bitmap = s_Snack;
+  uint16_t       grey   = rtc_button_state & kFoodSnackBit ? EPD_GRAY : EPD_BLACK;
+  display.drawBitmap( 0, 40, bitmap, kFoodIconWidth, kFoodIconHeight, grey );
+  drawString( centered, 40 + 75, String( "Snack" ), LEFT );
+  if( rtc_snack_count )
+  {
+      drawTime( centered, 40 + 65, &s_last_snack );
+      u8g2Fonts.setForegroundColor( EPD_GRAY );
+      u8g2Fonts.setFont( u8g2_font_helvB10_tf );
+      drawString( centered + 25, 56, String( rtc_snack_count ), LEFT );
+  }
+
+  u8g2Fonts.setForegroundColor( EPD_BLACK );
+  u8g2Fonts.setFont( u8g2_font_helvR08_tf );
+  w = getTextWidth( "Breakfast" );
+  centered = (kFoodIconWidth - w) / 2;  
+  bitmap = rtc_button_state & kFoodBreakfastBit ? s_BowlEmpty : s_BowlFull;
+  grey   = rtc_button_state & kFoodBreakfastBit ? EPD_GRAY : EPD_BLACK;
+  display.drawBitmap( kFoodIconWidth, 40, bitmap, kFoodIconWidth, kFoodIconHeight, grey );
+  drawString( kFoodIconWidth + centered + 6, 40 + 75, String( "Breakfast" ), LEFT );
+  if( rtc_button_state & kFoodBreakfastBit )
+      drawTime( kFoodIconWidth + centered + 14, 40 + 65, &s_last_breakfast );
+  
+  w = getTextWidth( "Lunch" );
+  centered = (kFoodIconWidth - w) / 2;  
+  bitmap = rtc_button_state & kFoodLunchBit ? s_BowlEmpty : s_BowlFull;
+  grey   = rtc_button_state & kFoodLunchBit ? EPD_GRAY : EPD_BLACK;
+  display.drawBitmap( kFoodIconWidth * 2, 40, bitmap, kFoodIconWidth, kFoodIconHeight, grey );
+  drawString( kFoodIconWidth * 2 + centered, 40 + 75, String( "Lunch" ), LEFT );
+  if( rtc_button_state & kFoodLunchBit )
+      drawTime( kFoodIconWidth * 2 + centered, 40 + 65, &s_last_lunch );
+
+  w = getTextWidth( "Dinner" );
+  centered = (kFoodIconWidth - w) / 2;  
+  bitmap = rtc_button_state & kFoodDinnerBit ? s_BowlEmpty : s_BowlFull;
+  grey   = rtc_button_state & kFoodDinnerBit ? EPD_GRAY : EPD_BLACK;
+  display.drawBitmap( kFoodIconWidth * 3, 40, bitmap, kFoodIconWidth, kFoodIconHeight, grey );
+  drawString( kFoodIconWidth * 3 + centered + 3, 40 + 75, String( "Dinner" ), LEFT );
+  if( rtc_button_state & kFoodDinnerBit )
+      drawTime( kFoodIconWidth * 3 + centered + 3, 40 + 65, &s_last_dinner );
+
+  // draw the date
+  if( draw_date )
+  {
+    uint16_t w = getTextWidth( s_date_str );
+    uint16_t centered = (display.width() - w) / 2;
+    drawString( centered, 30, s_date_str, LEFT );
+  }
+
+//  drawRSSI( 230, 14, s_wifi_signal );
+  drawBattery( 255, 10 );
+
+  // send to e-paper display
+  display.display();
+}
+
+
+
+
+//#########################################################################################
 
 uint8_t StartWiFi() 
 {
@@ -452,75 +594,10 @@ uint8_t StartWiFi()
 }
 
 
-//#########################################################################################
 void StopWiFi() 
 {
   WiFi.disconnect();
   WiFi.mode( WIFI_OFF );
-}
-
-
-void draw_epd( bool draw_date )
-{
-  Serial.println("EPD begun");
-
-  display.begin();
-  display.clearBuffer();
-  display.fillScreen( EPD_WHITE );
-
-  display.drawBitmap( 0, -1, s_folabs_logo, 128, 32, EPD_BLACK );
-
-  u8g2Fonts.setFont( u8g2_font_helvB10_tf );
-  drawString( 40, 12, "Far Out Labs", LEFT );
-
-//  u8g2Fonts.setForegroundColor( EPD_RED );
-//  u8g2Fonts.setFont( u8g2_font_helvR08_tf );
-//  drawString( 40 + 120, 10, "K6LOT-13", LEFT );
-
-  u8g2Fonts.setForegroundColor( EPD_BLACK );
-  u8g2Fonts.setFont( u8g2_font_helvR08_tf );
-
-  uint16_t w = getTextWidth( "Snack" );
-  uint16_t centered = (kFoodIconWidth - w) / 2;
-  const uint8_t* bitmap = s_Snack;
-  uint16_t       grey   = rtc_button_state & kFoodSnackBit ? EPD_GRAY : EPD_BLACK;
-  display.drawBitmap( 0, 40, bitmap, kFoodIconWidth, kFoodIconHeight, grey );
-  drawString( centered, 40 + 75, String( "Snack" ), LEFT );
-
-  w = getTextWidth( "Breakfast" );
-  centered = (kFoodIconWidth - w) / 2;  
-  bitmap = rtc_button_state & kFoodBreakfastBit ? s_BowlEmpty : s_BowlFull;
-  grey   = rtc_button_state & kFoodBreakfastBit ? EPD_GRAY : EPD_BLACK;
-  display.drawBitmap( kFoodIconWidth, 40, bitmap, kFoodIconWidth, kFoodIconHeight, grey );
-  drawString( kFoodIconWidth + centered + 6, 40 + 75, String( "Breakfast" ), LEFT );
-
-  w = getTextWidth( "Lunch" );
-  centered = (kFoodIconWidth - w) / 2;  
-  bitmap = rtc_button_state & kFoodLunchBit ? s_BowlEmpty : s_BowlFull;
-  grey   = rtc_button_state & kFoodLunchBit ? EPD_GRAY : EPD_BLACK;
-  display.drawBitmap( kFoodIconWidth * 2, 40, bitmap, kFoodIconWidth, kFoodIconHeight, grey );
-  drawString( kFoodIconWidth * 2 + centered, 40 + 75, String( "Lunch" ), LEFT );
-
-  w = getTextWidth( "Dinner" );
-  centered = (kFoodIconWidth - w) / 2;  
-  bitmap = rtc_button_state & kFoodDinnerBit ? s_BowlEmpty : s_BowlFull;
-  grey   = rtc_button_state & kFoodDinnerBit ? EPD_GRAY : EPD_BLACK;
-  display.drawBitmap( kFoodIconWidth * 3, 40, bitmap, kFoodIconWidth, kFoodIconHeight, grey );
-  drawString( kFoodIconWidth * 3 + centered + 3, 40 + 75, String( "Dinner" ), LEFT );
-
-  // draw the date
-  if( draw_date )
-  {
-    uint16_t w = getTextWidth( s_date_str );
-    uint16_t centered = (display.width() - w) / 2;
-    drawString( centered, 30, s_date_str, LEFT );
-  }
-
-//  drawRSSI( 230, 14, s_wifi_signal );
-  drawBattery( 255, 10 );
-
-  // send to e-paper display
-  display.display();
 }
 
 
